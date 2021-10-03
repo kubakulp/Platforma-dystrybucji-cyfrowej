5b86da2f978abd729d5c227b678251db03c3f62b
kubakulp <63971508+kubakulp@users.noreply.github.com>
2021-10-03 13:30:55 +0200

    caly projekt

---
+++ ProjektSztim/DataOnly/projekt_biblioteka.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: projekt
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `biblioteka`
+--
+
+LOCK TABLES `biblioteka` WRITE;
+/*!40000 ALTER TABLE `biblioteka` DISABLE KEYS */;
+INSERT INTO `biblioteka` VALUES ('1heart','123',1),('Assassin\'s Creed II','123',0),('Clive Barker\'s Jericho','123',1),('Far Cry 4','123',0),('Killzone 3','123',0),('Psychonauts','123',1),('Syberia II','123',1),('A New Beginning - Final Cut','123',0),('Alan Wake','123',0),('Anno 2070','123',1),('Amnesia: A Machine for Pigs','123',0),('- Arcane Raise -','123',1),('Assassins Creed Liberation HD','123',0),('1heart','Cras',1),('Airline Tycoon 2','Cras',0),('Amnesia: The Dark Descent','123',0),('- Arcane Raise -','1234',1),('A.D. 2044','1234',0),('Anno 2070','1234',1),('A New Beginning - Final Cut','Cras',0);
+/*!40000 ALTER TABLE `biblioteka` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-18 12:42:52
---
+++ ProjektSztim/DataOnly/projekt_uzytkownicy.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: projekt
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `uzytkownicy`
+--
+
+LOCK TABLES `uzytkownicy` WRITE;
+/*!40000 ALTER TABLE `uzytkownicy` DISABLE KEYS */;
+INSERT INTO `uzytkownicy` VALUES ('123',_binary '-µôJæDæ=ù\”\÷˘R!','siema123@greg.pl',4850.8),('1234',_binary '¶Zà\Ï2‰ßè,ÅåvJE©','abc@gmail.com',801.33),('12345',_binary 'qÜÀùöj\n˜©ß˘m\ÌÉ\œ','jak@zyc.pl',0),('123456',_binary '∑é≠G©\ZKƒí0ı\È','taktak@wp.pl',913.62),('adipiscing',_binary 'b¶óKg\œ\⁄;5å“á˘\ŸEJ','leo.in@ametluctus.com',1000),('Cras',_binary '|3Æu°˙˚\Ì<\ƒ\»\·>i','pede.Praesent@felis.org',2000),('Donec',_binary '8ô\√:\Îñ˚gtël±','Mauris@duinec.org',120),('iaculis',_binary 'Îä¶(ia\Ó\‚∑\”°2','neque.sed.sem@luctusaliquetodio.ca',0),('nonummy',_binary '˙@å©w7%\\\\\÷\«Ä]\Ÿ','enim@vulputatelacusCras.edu',3500);
+/*!40000 ALTER TABLE `uzytkownicy` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-18 12:42:52
